84e92164099611f5cdd778e6af909fc2721396bc
qitianshi <tianshi.qi@icloud.com>
2021-07-18 00:11:18 +0800

    Add run to line and threshold tolerance

--- movement/LineSquare.py
+++ movement/LineSquare.py
@@ -17,9 +17,13 @@ from .PIDLoop import PIDLoop
 
 class LinePosition:
     AHEAD = 0
-    BEHIND = 0
+    BEHIND = 1
 
 class LineSquare(PIDLoop):
+
+    MOVE_TO_LINE_SPEED = 250
+    LINE_WAIT_TIME = 75
+    THRESHOLD_TOLERANCE = 3
 
     def __init__(self,
                  leftThreshold: int,
@@ -65,11 +69,20 @@ class LineSquare(PIDLoop):
     def run(self):
 
         directionMultiplier = 1 if self.linePosition == LinePosition.AHEAD else -1
-        
-        while not ((self.leftSensor.reflection() == self.leftThreshold and self.leftMotor.speed() == 0) and (self.rightSensor.reflection() == self.rightThreshold and self.rightMotor.speed() == 0)):
+
+        # Before running PID loop, the robot drives forward until it reaches the line.
+        while not (self.leftSensor.reflection() < self.leftThreshold and self.rightSensor.reflection() < self.rightThreshold):
+            self.leftMotor.run(LineSquare.MOVE_TO_LINE_SPEED * directionMultiplier)
+            self.rightMotor.run(LineSquare.MOVE_TO_LINE_SPEED * directionMultiplier)
+        self.leftMotor.hold()
+        self.rightMotor.hold()
+        wait(LineSquare.LINE_WAIT_TIME)        # Stops for a short time to allow the motors to settle.
+
+        while not (self.leftSensor.reflection() in range(self.leftThreshold - LineSquare.THRESHOLD_TOLERANCE, self.leftThreshold + LineSquare.THRESHOLD_TOLERANCE + 1) and self.rightSensor.reflection() in range(self.rightThreshold - LineSquare.THRESHOLD_TOLERANCE, self.rightThreshold + LineSquare.THRESHOLD_TOLERANCE + 1)):
 
             self.leftMotor.run(self.leftPid.update(self.leftSensor.reflection() - self.leftThreshold) * directionMultiplier)
             self.rightMotor.run(self.rightPid.update(self.rightSensor.reflection() - self.rightThreshold) * directionMultiplier)
 
         self.leftMotor.hold()
         self.rightMotor.hold()
+        wait(LineSquare.LINE_WAIT_TIME)
