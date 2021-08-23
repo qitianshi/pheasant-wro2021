1e8440c696b9921a0db7e369de7e25983a4c0b88
qitianshi <tianshi.qi@icloud.com>
2021-08-23 18:45:58 +0800

    Add Logic class with programs for run logic

--- pheasant_utils/Logic.py
+++ pheasant_utils/Logic.py
@@ -2,45 +2,59 @@
 # Created on 9 Jul 2021 for Team Pheasant.
 # Copyright © 2021 Qi Tianshi. All rights reserved.
 
-# Functions for handling run logic.
+# For handling run logic.
 
 
 # pylint: disable=F0401
 from pybricks.parameters import Color                                       # type: ignore
 # pylint: enable=F0401
 
-def blockPlacement(surplusColor, houses):
-    
-    """
-    Gives the correct placement of blocks for each house and the storage battery.
+class Logic:
 
-    :param surplusColor: The color of the surplus energy.
-    :type surplusColor: Color
-    :param houses: The energy requirement of each house, arranged from left to right.
-    :type houses: ((Color, Color), (Color, Color), (Color, Color))
-    :return: The placement of energy blocks for each house and the storage battery, arranged from the left house to the 
-             right house, then the storage battery.
-    :rtype: [(Color, Color), (Color, Color), (Color, Color), (Color, Color)]
-    """
+    # Robot storage
+    blocks = []
 
-    result = []
+    # Run randomization
+    surplus = None
+    houses = {}
 
-    for i in houses:
-        if len(i) == 2:
-            result.append(i)
-        else:
-            # For the house that only has one requirement block, the other energy type will be the surplus.
-            result.append((i, surplusColor))
+    # Positions where energy blocks can be deposited (enum workaround).
+    class DepositPoint:
 
-    # To find the energy type that is only requested once.
-    flattenedHouses = (item for sublist in houses for item in sublist)
-    missingColor = None
-    for i in (Color.BLUE, Color.YELLOW, Color.GREEN):
-        if flattenedHouses.count(i) == 1:
-            missingColor = i
-            break
+        LEFT_HOUSE = 0
+        TOP_HOUSE = 1
+        RIGHT_HOUSE = 2
+        STORAGE = 3
 
-    # For the battery.
-    result.append((surplusColor, missingColor))
+    @classmethod
+    def blockDeposit(cls, point: DepositPoint):
+        
+        """
+        Gives the correct placement of blocks for each house and the storage battery.
 
-    return result
+        :param depositPoint: The requested deposit point.
+        :return: The placement of energy blocks for the requested deposit point.
+        :rtype: (Color, Color)
+        """
+
+        result = []
+
+        for i in cls.houses:
+            if len(i) == 2:
+                result.append(i)
+            else:
+                # For the house that only has one requirement block, the other energy type will be the surplus.
+                result.append((i, cls.surplus))
+
+        # To find the energy type that is only requested once.
+        flattenedHouses = (item for sublist in cls.houses for item in sublist)
+        missingColor = None
+        for i in (Color.BLUE, Color.YELLOW, Color.GREEN):
+            if flattenedHouses.count(i) == 1:
+                missingColor = i
+                break
+
+        # For the battery.
+        result.append((cls.surplus, missingColor))
+
+        return result
