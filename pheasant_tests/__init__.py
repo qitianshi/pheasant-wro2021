5e7f2b2a7b8f084655091a851276bcf535d5c835
qitianshi <tianshi.qi@icloud.com>
2021-08-28 12:40:57 +0800

    Add pheasant_tests

---
+++ pheasant_tests/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+# Created on 28 Aug 2021 for Team Pheasant.
+# Copyright © 2021 Qi Tianshi. All rights reserved.
+
+# Pheasant tests.
---
+++ pheasant_tests/test_RunLogic.py
@@ -0,0 +1,17 @@
+# test_RunLogic.py
+# Created on 28 Aug 2021 for Team Pheasant.
+# Copyright © 2021 Qi Tianshi. All rights reserved.
+
+# Unit tests for pheasant_utils.RunLogic.
+
+
+import unittest
+# from pheasant_utils import RunLogic                                         # pylint: disable=F0401
+
+# FIXME: Importing pheasant_utils fails because pybricks is not available in the default environment.
+
+class test_RunLogic(unittest.TestCase):
+    pass        # TODO: Add unit tests.
+
+if __name__ == '__main__':
+    unittest.main()
