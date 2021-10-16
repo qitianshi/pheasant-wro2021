7b3fa5d9f9a1c17770d832dacb6d1b0b63d0fa4e
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-10-16 22:19:40 +0800

    Move run execution to run.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env pybricks-micropython
-
 # main.py
 # Created on 6 Jul 2021 for Team Pheasant.
 # Copyright © 2021 Qi Tianshi. All rights reserved.
 
-# Main program.
+# Procedures for run.
 
 
 #region Start-up
@@ -64,32 +62,6 @@ if BRICK.battery.voltage() < 7750:      # In millivolts.
 #endregion
 
 #region Procedures
-
-# To initialize hardware and run variables when the robot starts from a save point on the field instead of the start
-# zone. Comment out the call to this function if running from the start zone.
-def partialRunStartupProcedure():
-
-    print("-" * 10, "Begin partialRunStartupProcedure", sep='\n')
-
-    # Delay for manual claw loading
-    utils.FrontClaw.lift()
-    utils.RearClaw.lift()
-    wait(10000)
-
-    # Claws
-    utils.RearClaw.loads = 2
-    utils.RearClaw.closeGate()
-    utils.FrontClaw.loads = 2
-    utils.FrontClaw.lift()
-
-    # Gyro
-    # GYRO.reset_angle(0)
-
-    # Run variables
-    # utils.RunLogic.undercarriageStorage = []
-    utils.RunLogic.houses = {utils.DepositPoint.LEFT_HOUSE: [],
-                             utils.DepositPoint.TOP_HOUSE: [],
-                             utils.DepositPoint.RIGHT_HOUSE: [utils.BlockColor.GREEN, utils.BlockColor.GREEN]}
 
 def scanHouseBlocksProcedure(thisHouse: utils.DepositPoint, gyroAngle: int, stopCondition, thenHoldMotors: bool):
 
@@ -583,21 +555,3 @@ def scanBlocksAtTopHouse():
                              lambda: LEFT_COLOR.color() != Color.BLACK and RIGHT_COLOR.color() != Color.BLACK, True)
 
 #endregion
-
-partialRunStartupProcedure()
-
-# moveToLeftHouse()
-# scanBlocksAtLeftHouse()
-# collectYellowSurplusAndLeftEnergy()
-# rotateSolarPanels()
-# collectYellowRightEnergy()
-# collectGreenSurplus()
-# collectGreenEnergy()
-# collectBlueSurplus()
-# collectBlueEnergy()
-# scanBlocksAtTopHouse()
-
-DepositEnergy(utils.DepositPoint.RIGHT_HOUSE, 0).run()
-
-while True:
-    wait(10000000)
---
+++ run.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env pybricks-micropython
+
+# run.py
+# Created on 16 Oct 2021 for Team Pheasant.
+# Copyright © 2021 Qi Tianshi. All rights reserved.
+
+# Executes robot run.
+
+
+from main import *                                              #pylint: disable=wildcard-import, unused-wildcard-import
+
+# To initialize hardware and run variables when the robot starts from a save point on the field instead of the start
+# zone. Comment out the call to this function if running from the start zone.
+def partialRunStartupProcedure():
+
+    print("-" * 10, "Begin partialRunStartupProcedure", sep='\n')
+
+    # Delay for manual claw loading
+    utils.FrontClaw.lift()
+    utils.RearClaw.lift()
+    wait(10000)
+
+    # Claws
+    utils.RearClaw.loads = 2
+    utils.RearClaw.closeGate()
+    utils.FrontClaw.loads = 2
+    utils.FrontClaw.lift()
+
+    # Gyro
+    GYRO.reset_angle(0)
+
+    # Run variables
+    utils.RunLogic.undercarriageStorage = []
+    utils.RunLogic.houses = {utils.DepositPoint.LEFT_HOUSE: [],
+                             utils.DepositPoint.TOP_HOUSE: [],
+                             utils.DepositPoint.RIGHT_HOUSE: []}
+
+# partialRunStartupProcedure()
+
+moveToLeftHouse()
+scanBlocksAtLeftHouse()
+collectYellowSurplusAndLeftEnergy()
+rotateSolarPanels()
+collectYellowRightEnergy()
+collectGreenSurplus()
+collectGreenEnergy()
+collectBlueSurplus()
+collectBlueEnergy()
+scanBlocksAtTopHouse()
+
+wait(1000)
